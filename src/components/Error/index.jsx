6b883e9c5f07972c8b74d44ae922518fd2d075f2
Kharismahardikaa <muhammadkharismam@gmail.com>
2021-12-04 22:06:24 +0800

    last commit

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Error.module.css';
+
+const Error = () => {
+    <div className={styles.container}>
+        <p className={styles.error}>Error, please reload the page</p>
+    </div>
+}
+
+export default Error
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+
+import styles from './Navbar.module.css';
+
+import { CATEGORIES } from '../../constants/categories';
+
+import newsIcon from '../../assets/news-icon.svg';
+
+const Navbar = () => {
+
+    const [selected, setSelected] = useState('')
+
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.navIconWrapper}>
+                <img src={newsIcon} alt="navbar icon" className={styles.navIcon}/>
+                <h1 className={styles.navTitle}>News</h1>
+            </div>
+
+            <div className={styles.categories}>
+                {CATEGORIES.map((category, index) => {
+                    return (
+                        <Link
+                            key={index}
+                            onClick={() => setSelected(category.name)}
+                            to={`/${category.slug}`}
+                            className={classnames(styles.category, {
+                                [styles.selected]: selected === category.name
+                            })}
+                        >
+                            {category.name}
+                        </Link>
+                    )
+                })}
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
